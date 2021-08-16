10213c556e7e4cbc804464af1be74118b09909d6
praneshv <pranesh.nv@gmail.com>
2021-08-16 13:56:33 +0530

    Update __main__.py

--- EC2Creation/__main__.py
+++ EC2Creation/__main__.py
@@ -9,12 +9,20 @@ ami = aws.ec2.get_ami(most_recent="true",
 group = aws.ec2.SecurityGroup('webserver-secgrp',
     description='Enable HTTP access',
     ingress=[
-        { 'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0'] }
+        { 'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0'] },
+        { 'protocol': 'tcp', 'from_port': 80, 'to_port': 80, 'cidr_blocks': ['0.0.0.0/0'] }
     ])
+
+user_data = """
+#!/bin/bash
+echo "Hello, World!" > index.html
+nohup python -m SimpleHTTPServer 80 &
+"""
 
 server = aws.ec2.Instance('webserver-www',
     instance_type=size,
     vpc_security_group_ids=[group.id], # reference security group from above
+    user_data=user_data,
     ami=ami.id)
 
 pulumi.export('publicIp', server.public_ip)
